ca05ad80ac9ff3e9165682866062943636388f79
yellowflash7 <prashantsingh918@gmail.com>
2017-06-26 05:21:08 -0400

    First commit

--- database_setup.py
+++ database_setup.py
@@ -1,54 +1,57 @@
-
 import sys
-from sqlalchemy import Column,ForeignKey,Integer,String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
-    name = Column(String(80),nullable = False)
-    id = Column(Integer,primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250))
     picture = Column(String(250))
 
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-    name = Column(String(80),nullable = False)
-    id = Column(Integer,primary_key = True)
-    user_id = Column(Integer,ForeignKey('user.id'))
-    user=relationship(User)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
         return{
-        'id' : self.id,
-        'name' :self.name,
+            'id': self.id,
+            'name': self.name,
         }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-    name = Column(String(80),nullable = False)
-    id = Column(Integer,primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    user_id = Column(Integer,ForeignKey('user.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     restaurant = relationship(Restaurant)
-    user=relationship(User)
+    user = relationship(User)
 
     @property
     def serialize(self):
         return {
-        'name' :self.name,
-        'description':self.description,
-        'id' :self.id,
-        'price' :self.price,
-        'course' :self.course,
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
         }
+
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
